eb22d94982694550fef7e2c912f0a90e31c5a8f9
Petar Yanakiev <petaryanakiev.py@gmail.com>
2026-05-07 10:15:03 +0300

    Add buffering class that emits captured mic input through async streams

---
+++ Translator/Translator/Interpreter/Audio/MicCapture.swift
@@ -0,0 +1,43 @@
+//
+//  MicCapture.swift
+//  Translator
+//
+//  Created by Petar Yanakiev on 7.05.26.
+//
+
+import AVFoundation
+
+final class MicCapture: @unchecked Sendable {
+    private let engine = AVAudioEngine()
+    private var continuation: AsyncStream<AVAudioPCMBuffer>.Continuation?
+
+    let buffers: AsyncStream<AVAudioPCMBuffer>
+
+    init() {
+        var cont: AsyncStream<AVAudioPCMBuffer>.Continuation!
+        self.buffers = AsyncStream { c in cont = c }
+        self.continuation = cont
+    }
+
+    var inputFormat: AVAudioFormat { engine.inputNode.inputFormat(forBus: 0) }
+
+    func start() throws {
+        let input = engine.inputNode
+        try? input.setVoiceProcessingEnabled(true)
+
+        let format = input.inputFormat(forBus: 0)
+        input.removeTap(onBus: 0)
+        input.installTap(onBus: 0, bufferSize: 4096, format: format) { [weak self] buffer, _ in
+            self?.continuation?.yield(buffer)
+        }
+
+        engine.prepare()
+        try engine.start()
+    }
+
+    func stop() {
+        engine.inputNode.removeTap(onBus: 0)
+        if engine.isRunning { engine.stop() }
+        continuation?.finish()
+    }
+}
