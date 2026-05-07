0a2c43bc9a346b0afad984ee836eb2e874774aa7
Petar Yanakiev <petaryanakiev.py@gmail.com>
2026-05-07 10:04:19 +0300

    Initialize an empty project in Xcode

---
+++ Translator/Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Petar Yanakiev on 7.05.26.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
